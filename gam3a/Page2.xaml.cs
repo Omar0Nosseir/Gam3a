aa6d490115a43208cad69689c8566adb21176538
Omar0Nosseir <omarerror2007@gmail.com>
2023-11-30 12:20:17 +0200

    Version1

---
+++ gam3a/Page2.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gam3a
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void save_btn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void open_btn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Students st = new Students();
+            st.peo_namem = name_tb.Text;
+            st.peo_age = age_tb.Text;
+            st.peo_address= address_tb.Text;
+            st.Dep_name = department_tb.Text;
+            db.Students .Add(st);
+            db.Savechanges();
+
+            MessageBox.Show("successfully aded");
+
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Page1 pg = new Page1();
+            NavigationService.Navigate(pg);
+        }
+    }
+}
